d37bf7eda8fd6600b0e8c75332d371af3bd0d3da
blakepuckett11 <blakepuckett@u.boisestate.edu>
2025-03-11 17:13:38 -0600

    Lab Complete

--- README.md
+++ README.md
@@ -2,23 +2,16 @@
 
 ## Overview
 
-TODO: Write a project description
+This lab is a fun bouncing ball simulator with an "Evil Circle" in the middle that deletes balls upon getting hit!
+
+
+## Running
+To run this lab, simply right click on the "index.html" file and hit the "Open with Live Server" option.
+
 
 ## Sources and Credits
 
-TODO: You must credit the sources and authors of any code, libraries, or other
-assets you use in your project. If you leave this section blank, your project
-will be considered in violation of the Academic Honesty policy unless you truly
-created everything from scratch with no outside help. If you need to use a
-source that you cannot credit (e.g. a classmate's work), you must get explicit
-permission from your instructor.
 
-A simple bulleted list below is sufficient. For example:
+- JavaScript Function refresher: https://www.w3schools.com/jsref/jsref_constructor_class.asp
 
-- Bootstrap: https://getbootstrap.com/
-- jQuery: https://jquery.com/
-- Background image: https://unsplash.com/photos/...
-- Sound effects: https://freesound.org/people/...
-- Icons: https://fontawesome.com/
-- Fonts: https://fonts.google.com/
-- etc.
+- JavaScript single line if statements: https://stackoverflow.com/questions/8860654/javascript-single-line-if-statement-best-syntax-this-alternative
--- js/main.js
+++ js/main.js
@@ -17,18 +17,33 @@ function random(min, max) {
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
+// Ball count disp for html
+const ballCountDisp = document.getElementById("ballCount");
+// max amount
+let ballCount = 25; 
+// get ballCount val and apply it to the html
+ballCountDisp.textContent = ballCount;
 
-class Ball {
-  constructor(x, y, velX, velY, color, size) {
+// Shape class
+class Shape {
+  constructor(x, y, velX, velY) {
     this.x = x;
     this.y = y;
     this.velX = velX;
     this.velY = velY;
+  }
+}
+// Ball class extending Shape
+class Ball extends Shape {
+  constructor(x, y, velX, velY, color, size) {
+    super(x, y, velX, velY);
     this.color = color;
     this.size = size;
+    this.exists = true;
   }
 
   draw() {
+    if (!this.exists) return;
     ctx.beginPath();
     ctx.fillStyle = this.color;
     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
@@ -36,29 +51,21 @@ class Ball {
   }
 
   update() {
-    if (this.x + this.size >= width) {
-      this.velX = -Math.abs(this.velX);
+    if (!this.exists) return;
+    if (this.x + this.size >= width || this.x - this.size <= 0) {
+      this.velX = -this.velX;
     }
-
-    if (this.x - this.size <= 0) {
-      this.velX = Math.abs(this.velX);
+    if (this.y + this.size >= height || this.y - this.size <= 0) {
+      this.velY = -this.velY;
     }
-
-    if (this.y + this.size >= height) {
-      this.velY = -Math.abs(this.velY);
-    }
-
-    if (this.y - this.size <= 0) {
-      this.velY = Math.abs(this.velY);
-    }
-
     this.x += this.velX;
     this.y += this.velY;
   }
 
   collisionDetect() {
+    if (!this.exists) return;
     for (const ball of balls) {
-      if (!(this === ball)) {
+      if (ball.exists && this !== ball) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
@@ -70,14 +77,70 @@ class Ball {
     }
   }
 }
+// Evil Circle class
+class EvilCircle extends Shape {
+  // constructor
+  constructor(x, y) {
+    super(x, y, 20, 20);
+    this.color = "white";
+    this.size = 10;
+    // event listener
+    window.addEventListener("keydown", (e) => {
+      switch (e.key) {
+        case "a":
+          this.x -= this.velX;
+          break;
+        case "d":
+          this.x += this.velX;
+          break;
+        case "w":
+          this.y -= this.velY;
+          break;
+        case "s":
+          this.y += this.velY;
+          break;
+      }
+    });
+  }
+  // evil circle funcs
+
+  draw() {
+    ctx.beginPath();
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+
+  checkBounds() {
+    if (this.x + this.size >= width) this.x = width - this.size;
+    if (this.x - this.size <= 0) this.x = this.size;
+    if (this.y + this.size >= height) this.y = height - this.size;
+    if (this.y - this.size <= 0) this.y = this.size;
+  }
+
+  collisionDetect() {
+    for (const ball of balls) {
+      if (ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          // ball gets deleted and ballCount is decremented
+          ball.exists = false;
+          ballCount--;
+          ballCountDisp.textContent = ballCount;
+        }
+      }
+    }
+  }
+}
 
 const balls = [];
-
 while (balls.length < 25) {
   const size = random(10, 20);
   const ball = new Ball(
-    // ball position always drawn at least one ball width
-    // away from the edge of the canvas, to avoid drawing errors
     random(0 + size, width - size),
     random(0 + size, height - size),
     random(-7, 7),
@@ -89,6 +152,8 @@ while (balls.length < 25) {
   balls.push(ball);
 }
 
+const evilCircle = new EvilCircle(width / 2, height / 2);
+
 function loop() {
   ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
   ctx.fillRect(0, 0, width, height);
@@ -98,6 +163,10 @@ function loop() {
     ball.update();
     ball.collisionDetect();
   }
+  // loop call evilCircle functions
+  evilCircle.draw();
+  evilCircle.checkBounds();
+  evilCircle.collisionDetect();
 
   requestAnimationFrame(loop);
 }
